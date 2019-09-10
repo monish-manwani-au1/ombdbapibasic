0bb60f3b01a3e485c9ef4409b4bcce5f0656b190
Manish verma <rohitvermagzp@gmail.com>
2019-09-10 20:08:25 +0530

    manishstuff

---
+++ manish/omdb2/public/delete.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    $('.deletebtn1').on('click', function(){
+       console.log("hello");
+       var id = $(this).prop("value");
+       console.log(id);
+    $.ajax({
+       url: "/myReviews/delete/"+id,
+       type: 'DELETE',
+       dataType: 'json',
+       success: function(data){
+             window.location = "/myReviews";
+       }
+    });
+
+});
+};
---
+++ manish/omdb2/public/main.js
@@ -0,0 +1,73 @@
+$(document).ready(function ($) {
+    //api url
+    //var apiUrl = "http://www.omdbapi.com/";
+   
+    //Event binding on enter button pressed inside movie search input box
+    $('#searchText').keyup(function () {
+        if (event.keyCode == 13) {
+            $('#btn').click();
+        } else {
+            return false
+        }
+    });
+  
+    //Event binding on search button click
+    $('#btn').click(function () {
+        var searchString = $('#searchText').val();
+        var searchUrl ='http://www.omdbapi.com/?s='+searchString+'&apikey=9370112b'
+  
+    //    $('#search-results .movie-list').empty();
+  
+        //Remove previous searched Movie
+    //    $('#search-results .movie').empty();
+       
+        //Get Movie list from api using search string
+        $.ajax({
+            url: searchUrl,
+            type: 'GET',
+            dataType: 'text',
+            // statusCode: {
+            //     403: function () {
+            //         $('#search-results').html('HTTP 403 Forbidden!');
+            //     }
+            // },
+            success: function (response) {
+                var result=JSON.parse(response);
+  
+                //check if no movie found
+                if (result.Response == "False") {
+                    $('#movies').html("<li class='list-group-item'>"+result.Error+"</li>");
+                }else{
+                    var MovieArray=result.Search;
+                    var resultLength=MovieArray.length;
+               //     movieListHtml="";
+                    var output = '';
+                    MovieArray.forEach(function(movie){
+                        output += `
+                                <div class="col-md-3">
+                                    <div class="well text-center">
+                                    <img src="${movie.Poster}">
+                                    <h5>${movie.Title}</h5>
+                                    <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                                    </div>
+                                </div>
+                             `;
+                    });
+                    $('#movies').html(output);
+                   
+                    //binding event on movie list
+                //    addEventOnMovieList();
+                }
+            }
+        });//ajax closed
+ });
+  
+ });
+
+ function movieSelected(id){
+    sessionStorage.setItem('movieId', id);
+    window.location = '/movieInfo';
+    return false;
+  }
+// function to show movie details and call function the movie.hbs
+ 
