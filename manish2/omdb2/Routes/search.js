f8fbd52e97d223cdbe2399d5606c3496d242e963
karthik-rao-au3 <karthikb786@gmail.com>
2019-09-12 00:39:07 +0530

    CSS_file is fixed

--- manish2/omdb2/Routes/search.js
+++ manish2/omdb2/Routes/search.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/search", function(req, res){
-    if(req.session.loggedIn == true){
-        res.render("omdb",{
+router.get("/search", function(req, res) {
+    if (req.session.loggedIn == true) {
+        res.render("omdb", {
             title: "omdb search",
-            style: "./omdb.css",
+            style: "/omdb.css",
             script: "/main.js"
-            
+
         });
-    }else{
+    } else {
         res.redirect("/");
     }
 });
